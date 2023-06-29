512fd59f1439255b7e786f59842e96406986005e
yuxinyu <yuxinyu@stu.pku.edu.cn>
2023-06-29 20:59:45 +0800

    Assignment1 without tags and dinocpu.sif

---
+++ dinocpu-assignment1/src/main/scala/components/alucontrol.scala
@@ -0,0 +1,70 @@
+// This file contains ALU control logic.
+
+package dinocpu.components
+
+import chisel3._
+import chisel3.util._
+
+/**
+ * The ALU control unit
+ *
+ * Input:  aluop        Specifying the type of instruction using ALU
+ *                          . 0 for none of the below
+ *                          . 1 for 64-bit R-type
+ *                          . 2 for 64-bit I-type
+ *                          . 3 for 32-bit R-type
+ *                          . 4 for 32-bit I-type
+ *                          . 5 for non-arithmetic instruction types that uses ALU (auipc/jal/jarl/Load/Store)
+ * Input:  funct7       The most significant bits of the instruction.
+ * Input:  funct3       The middle three bits of the instruction (12-14).
+ *
+ * Output: operation    What we want the ALU to do.
+ *
+ * For more information, see Section 4.4 and A.5 of Patterson and Hennessy.
+ * This is loosely based on figure 4.12
+ */
+class ALUControl extends Module {
+  val io = IO(new Bundle {
+    val aluop     = Input(UInt(3.W))
+    val funct7    = Input(UInt(7.W))
+    val funct3    = Input(UInt(3.W))
+
+    val operation = Output(UInt(5.W))
+  })
+
+  io.operation := "b11111".U // Invalid
+
+  // Your code goes here
+  when(io.aluop === 1.U){ //64-bit R-type
+    when((io.funct3 === "b000".U)&(io.funct7 === "b0000000".U)) {io.operation := "b01100".U} //add
+    .elsewhen((io.funct3 === "b000".U) & (io.funct7 === "b0100000".U)) {io.operation := "b01110".U} //sub
+    .elsewhen((io.funct3 === "b000".U) & (io.funct7 === "b0000001".U)) {io.operation := "b10000".U} //mul
+    .elsewhen((io.funct3 === "b001".U) & (io.funct7 === "b0000000".U)) {io.operation := "b00110".U} //sll
+    .elsewhen((io.funct3 === "b001".U) & (io.funct7 === "b0000001".U)) {io.operation := "b10010".U} //mulh
+    .elsewhen((io.funct3 === "b010".U) & (io.funct7 === "b0000000".U)) {io.operation := "b01010".U} //slt
+    .elsewhen((io.funct3 === "b010".U) & (io.funct7 === "b0000001".U)) {io.operation := "b11100".U} //mulhsu
+    .elsewhen((io.funct3 === "b011".U) & (io.funct7 === "b0000000".U)) {io.operation := "b01011".U} //sltu
+    .elsewhen((io.funct3 === "b011".U) & (io.funct7 === "b0000001".U)) {io.operation := "b10011".U} //mulhu
+    .elsewhen((io.funct3 === "b100".U) & (io.funct7 === "b0000000".U)) {io.operation := "b00010".U} //xor
+    .elsewhen((io.funct3 === "b100".U) & (io.funct7 === "b0000001".U)) {io.operation := "b10100".U} //div
+    .elsewhen((io.funct3 === "b101".U) & (io.funct7 === "b0000000".U)) {io.operation := "b01000".U} //srl
+    .elsewhen((io.funct3 === "b101".U) & (io.funct7 === "b0100000".U)) {io.operation := "b00100".U} //sra
+    .elsewhen((io.funct3 === "b101".U) & (io.funct7 === "b0000001".U)) {io.operation := "b10101".U} //divu
+    .elsewhen((io.funct3 === "b110".U) & (io.funct7 === "b0000000".U)) {io.operation := "b00001".U} //or
+    .elsewhen((io.funct3 === "b110".U) & (io.funct7 === "b0000001".U)) {io.operation := "b11000".U} //rem
+    .elsewhen((io.funct3 === "b111".U) & (io.funct7 === "b0000000".U)) {io.operation := "b00000".U} //and
+    .elsewhen((io.funct3 === "b111".U) & (io.funct7 === "b0000001".U)) {io.operation := "b11001".U} //remu
+  }
+  .elsewhen(io.aluop === 3.U){ //32-bit R-type
+    when((io.funct3 === "b000".U) & (io.funct7 === "b0000000".U)) {io.operation := "b01101".U} //addw
+    .elsewhen((io.funct3 === "b000".U) & (io.funct7 === "b0100000".U)) {io.operation := "b01111".U} //subw
+    .elsewhen((io.funct3 === "b000".U) & (io.funct7 === "b0000001".U)) {io.operation := "b10001".U} //mulw
+    .elsewhen((io.funct3 === "b001".U) & (io.funct7 === "b0000000".U)) {io.operation := "b00111".U} //sllw
+    .elsewhen((io.funct3 === "b100".U) & (io.funct7 === "b0000001".U)) {io.operation := "b10110".U} //divw
+    .elsewhen((io.funct3 === "b101".U) & (io.funct7 === "b0000000".U)) {io.operation := "b01001".U} //srlw
+    .elsewhen((io.funct3 === "b101".U) & (io.funct7 === "b0100000".U)) {io.operation := "b00101".U} //sraw
+    .elsewhen((io.funct3 === "b101".U) & (io.funct7 === "b0000001".U)) {io.operation := "b10111".U} //divuw
+    .elsewhen((io.funct3 === "b110".U) & (io.funct7 === "b0000001".U)) {io.operation := "b11010".U} //remw
+    .elsewhen((io.funct3 === "b111".U) & (io.funct7 === "b0000001".U)) {io.operation := "b11011".U} //remuw
+  }
+}
---
+++ dinocpu-assignment1/src/main/scala/single-cycle/cpu.scala
@@ -0,0 +1,85 @@
+// This file is where all of the CPU components are assembled into the whole CPU
+
+package dinocpu
+
+import chisel3._
+import chisel3.util._
+import dinocpu.components._
+
+/**
+ * The main CPU definition that hooks up all of the other components.
+ *
+ * For more information, see section 4.4 of Patterson and Hennessy
+ * This follows figure 4.21
+ */
+class SingleCycleCPU(implicit val conf: CPUConfig) extends BaseCPU {
+  // All of the structures required
+  val pc              = dontTouch(RegInit(0.U(64.W)))
+  val control         = Module(new Control())
+  val registers       = Module(new RegisterFile())
+  val aluControl      = Module(new ALUControl())
+  val alu             = Module(new ALU())
+  val immGen          = Module(new ImmediateGenerator())
+  val controlTransfer = Module(new ControlTransferUnit())
+  val (cycleCount, _) = Counter(true.B, 1 << 30)
+
+  control.io := DontCare
+  registers.io := DontCare
+  aluControl.io := DontCare
+  alu.io := DontCare
+  immGen.io := DontCare
+  controlTransfer.io := DontCare
+  io.dmem <> DontCare
+  //immGen.io <> DontCare
+  //controlTransfer.io <> DontCare
+  //io.dmem <> DontCare
+
+  //FETCH
+  io.imem.address := pc
+  io.imem.valid := true.B
+
+  val instruction = Wire(UInt(32.W))
+  when ((pc % 8.U) === 4.U) {
+    instruction := io.imem.instruction(63, 32)
+  } .otherwise {
+    instruction := io.imem.instruction(31, 0)
+  }
+  
+  // Your code goes here
+  control.io.opcode := instruction(6,0)
+  
+  registers.io.readreg1 := instruction(19,15)
+  registers.io.readreg2 := instruction(24,20)
+  registers.io.writereg := instruction(11,7)
+  registers.io.writedata := alu.io.result
+  registers.io.wen := (registers.io.writereg =/= 0.U) & (control.io.writeback_valid === 1.U)
+
+  aluControl.io.aluop := control.io.aluop 
+  aluControl.io.funct3 := instruction(14,12)
+  aluControl.io.funct7 := instruction(31,25)
+
+  alu.io.operation := aluControl.io.operation
+  alu.io.operand1 := registers.io.readdata1
+  alu.io.operand2 := registers.io.readdata2
+
+  pc := pc + 4.U
+}
+
+/*
+ * Object to make it easier to print information about the CPU
+ */
+object SingleCycleCPUInfo {
+  def getModules(): List[String] = {
+    List(
+      "dmem",
+      "imem",
+      "control",
+      "registers",
+      "csr",
+      "aluControl",
+      "alu",
+      "immGen",
+      "controlTransfer"
+    )
+  }
+}
